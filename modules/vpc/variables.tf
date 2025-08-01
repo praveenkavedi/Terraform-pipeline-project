3f9b5d59e591effdf0a1de7289a2d6467635995e
praveen <praveen@praveens-MacBook-Pro.local>
2025-08-01 18:34:55 +0530

    modified the cidr datatype

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,6 +1,6 @@
 variable "vpc_cidr" {
     description = "CIDR block for the VPC"
-    type        = number
+    type        = string
   
 }
 
@@ -11,7 +11,7 @@ variable "env" {
 
 variable "subnet_cidr" {
     description = "CIDR block for the subnet"
-    type        = number
+    type        = string
 }
 
 variable "az" {
